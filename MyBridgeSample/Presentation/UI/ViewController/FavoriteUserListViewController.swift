6df75ead9f199f5aa6264ef735070e2fc66c4886
matsuda <kosukematsuda@gmail.com>
2020-05-22 02:33:01 +0900

    Add favorite user list

---
+++ MyBridgeSample/Presentation/UI/ViewController/FavoriteUserListViewController.swift
@@ -0,0 +1,68 @@
+//
+//  FavoriteUserListViewController.swift
+//  MyBridgeSample
+//
+//  Created by Kosuke Matsuda on 2020/05/22.
+//  Copyright © 2020 Kosuke Matsuda. All rights reserved.
+//
+
+import UIKit
+
+final class FavoriteUserListViewController: UIViewController {
+
+    // UIs
+    @IBOutlet private weak var tableView: UITableView!
+    @IBOutlet private weak var searchBar: UISearchBar!
+
+    var users: [User] = []
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        prepareTableView()
+        loadData()
+    }
+}
+
+
+// MARK: - private
+
+extension FavoriteUserListViewController {
+    private func prepareTableView() {
+    }
+
+    private func loadData() {
+        guard let path = Bundle.main.path(forResource: "users", ofType: "json") else { return }
+        let url = URL(fileURLWithPath: path)
+        guard let data = try? Data(contentsOf: url) else { return }
+        guard let userList = try? JSONDecoder().decode(GitHubUserList.self, from: data) else {
+            return
+        }
+        users = userList.users.map(User.init(gitHubUser:))
+    }
+}
+
+
+// MARK: - UITableViewDataSource
+
+extension FavoriteUserListViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return users.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "Cell")
+        if cell == nil {
+            cell = UITableViewCell(style: .default, reuseIdentifier: "Cell")
+        }
+        let user = users[indexPath.row]
+        cell?.textLabel?.text = user.name
+        return cell!
+    }
+}
+
+
+// MARK: - UITableViewDelegate
+
+extension FavoriteUserListViewController: UITableViewDelegate {
+}
--- MyBridgeSample/Presentation/UI/ViewController/ViewController.swift
+++ MyBridgeSample/Presentation/UI/ViewController/ViewController.swift
@@ -14,13 +14,27 @@ class ViewController: UIViewController {
         super.viewDidLoad()
 
         title = "GitHub Stars"
-        loadSearchUserList()
+//        loadSearchUserList()
+        loadFavoriteUserList()
     }
 }
 
 extension ViewController {
     private func loadSearchUserList() {
         let ident = String(describing: SearchUserListViewController.self)
+        let vc = UIStoryboard(name: ident, bundle: nil).instantiateInitialViewController()!
+        addChild(vc)
+        view.addSubview(vc.view)
+        vc.view.translatesAutoresizingMaskIntoConstraints = false
+        vc.view.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        vc.view.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        view.bottomAnchor.constraint(equalTo: vc.view.bottomAnchor).isActive = true
+        view.trailingAnchor.constraint(equalTo: vc.view.trailingAnchor).isActive = true
+        vc.didMove(toParent: self)
+    }
+
+    private func loadFavoriteUserList() {
+        let ident = String(describing: FavoriteUserListViewController.self)
         let vc = UIStoryboard(name: ident, bundle: nil).instantiateInitialViewController()!
         addChild(vc)
         view.addSubview(vc.view)
